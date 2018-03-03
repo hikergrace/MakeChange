55eaa400b921d1b843bba360e0409536eda2d1c9
Grace Carter <graciec@gmail.com>
2018-03-02 19:19:58 -0700

    Completed adding Scanners to ask for money, completed if/else if/else, started makeChange method

--- README.md
+++ README.md
@@ -6,10 +6,18 @@ Program to calculate change.
 Technologies used:
 *Java Expressions (this is up to me)
 
-###Project Description
+#### Project Description
+This cash register project asks the user for 1) how much an item costs, 2) how much money they are giving me.
+The app will then return the amount of change needed in bills and coins. If the customer pays an exact amount, 
+they app will return a "thank you" statement. 
+If the customer owes more money, the app will let them know how much. 
 
-### How to Run This Project
+
+#### How to Run This Project
+Click run.
+Type in the amount the item costs, using two decimal places and no dollar sign, and click ENTER. 
+Ex. for an item that is $5.75, type 5.75; for an item that is $5, type in 5.00
 
 
-###Lessons Learned
+#### Lessons Learned
 
--- src/com/skilldistillery/projects/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/projects/makechange/MakeChangeApp.java
@@ -8,21 +8,33 @@ public class MakeChangeApp {
 		Scanner keyboard = new Scanner(System.in);
 		
 		//asking the customer the cost of the item
-		System.out.print("How much is the item? ");
+		System.out.print("How much is the item? (example: if it is $5, type 5.00)");
 		double itemCost = keyboard.nextDouble();
 		
 		//The user is then prompted asking how much money was tendered by the customer.
-		System.out.print("How much money are you giving me? ");
+		System.out.print("How much money are you giving me? ex. 6.00 ");
 		double custPay = keyboard.nextDouble();
 		
 		//calculates the change to give back to the customer
 		double changeToCust = custPay - itemCost;
 		
 		if(custPay < itemCost) {
-			System.out.println("you owe me");
-		} else {
+			System.out.println("You still owe me $" + (itemCost - custPay));
+		} else if (custPay > itemCost ){
+			//call a makeChange method
+			makeChange(changeToCust);
 		System.out.print("Change: " + changeToCust);
+		} else {
+			System.out.println("Thank you for exact payment.");
 		}
+	}
+
+		//makeChange method goes here, need to use division and modulus
+	public static void makeChange(double changeToCust) {
+		// convert itemCost and custPay to int: 
+		int twenty = 2000, ten = 1000, five = 500, one = 100, quarter = 25, dime = 10, nickel = 5;
+		int itemCostInt = (int)(changeToCust * 100);
+		
 		
 	}
 }
