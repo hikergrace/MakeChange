5d5153add72140586047599ccdec167672a87887
Grace Carter <graciec@gmail.com>
2018-03-02 18:28:00 -0700

    Beginning code for MakeChangeApp

--- src/com/skilldistillery/projects/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/projects/makechange/MakeChangeApp.java
@@ -1,5 +1,28 @@
 package com.skilldistillery.projects.makechange;
 
-public class MakeChangeApp {
+import java.util.Scanner;
 
+public class MakeChangeApp {
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+		
+		//asking the customer the cost of the item
+		System.out.print("How much is the item? ");
+		double itemCost = keyboard.nextDouble();
+		
+		//The user is then prompted asking how much money was tendered by the customer.
+		System.out.print("How much money are you giving me? ");
+		double custPay = keyboard.nextDouble();
+		
+		//calculates the change to give back to the customer
+		double changeToCust = custPay - itemCost;
+		
+		if(custPay < itemCost) {
+			System.out.println("you owe me");
+		} else {
+		System.out.print("Change: " + changeToCust);
+		}
+		
+	}
 }
